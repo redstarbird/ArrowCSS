cd57b0de2739613d83e00b7ae74d993662cafb54
redstarbird <contact@redstarbird.com>
2026-05-17 20:52:40 +0100

    Add testing framework with basic assertions and empty simple parse test file

---
+++ test/SimpleParseTest.c
@@ -0,0 +1,12 @@
+#include "testing.h"
+
+void TestSimpleParse(void)
+{
+    printf("Running TestSimpleParse...\n");
+}
+
+int main(void)
+{
+    TestSimpleParse();
+    return 0;
+}
---
+++ test/testing.h
@@ -0,0 +1,29 @@
+#ifndef TESTING_H
+#define TESTING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/** @brief Assert that a condition is true */
+#define ASSERT_TRUE(condition, message)                                         \
+    do                                                                          \
+    {                                                                           \
+        if (!(condition))                                                       \
+        {                                                                       \
+            fprintf(stderr, "[FAIL] %s:%d: %s\n", __FILE__, __LINE__, message); \
+            exit(1);                                                            \
+        }                                                                       \
+    } while (0)
+
+/** @brief Assert that two values are equal */
+#define ASSERT_EQUALS(expected, actual, message)                                                                                      \
+    do                                                                                                                                \
+    {                                                                                                                                 \
+        if ((expected) != (actual))                                                                                                   \
+        {                                                                                                                             \
+            fprintf(stderr, "[FAIL] %s:%d: %s (Expected %d, got %d)\n", __FILE__, __LINE__, message, (int)(expected), (int)(actual)); \
+            exit(1);                                                                                                                  \
+        }                                                                                                                             \
+    } while (0)
+
+#endif // TESTING_H
