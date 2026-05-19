1550765bfd039f2e101a41e6bf1d07b56d98017b
redstarbird <contact@redstarbird.com>
2026-05-19 10:22:16 +0100

    Add ArrowAllocator struct to allow users to define their own memory allocation functions

--- include/arrowcss.h
+++ include/arrowcss.h
@@ -4,4 +4,10 @@
 #include "../src/CSSAST.h"
 #include "../src/CSSParser.h"
 
+typedef struct ArrowAllocator
+{
+    void *(*alloc)(size_t size, void *user_data);
+    void (*free)(void *ptr, void *user_data);
+} ArrowAllocator;
+
 #endif
