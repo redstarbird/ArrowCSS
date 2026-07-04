fd69adf352c3a775332146b2de5231f654f69300
redstarbird <contact@redstarbird.com>
2026-07-04 12:49:22 +0100

    refactor(test): move testing readDataFromFile function body from testing.h into testing.c

--- test/testing.h
+++ test/testing.h
@@ -26,51 +26,6 @@
         }                                                                                                                             \
     } while (0)
 
-char *readDataFromFile(char *filepath, size_t *size)
-{
-    // Open file in read binary mode
-    FILE *filePTR = fopen(filepath, "rb");
-
-    // If there was an error opening the file return NULL
-    if (filePTR == NULL)
-    {
-        printf("Unable to open file: %s\n", filepath);
-        return NULL;
-    }
-
-    // Find length of file
-    fseek(filePTR, 0, SEEK_END);
-    long int length = ftell(filePTR);
-
-    // Check the file size pointer is provided before attempting to set it
-    if (size != NULL)
-    {
-        // Set the given size pointer to the length
-        *size = length;
-    }
-
-    // Go back to the start of the file
-    fseek(filePTR, 0, SEEK_SET);
-
-    // Create buffer with the required length including a null terminator
-    char *buffer = malloc(sizeof(char) * (length + 1));
-
-    if (buffer == NULL)
-    {
-        printf("Error creating buffer\n");
-        return NULL;
-    }
-
-    // Read entire file into buffer
-    fread(buffer, 1, length, filePTR);
-
-    // Close the file
-    fclose(filePTR);
-
-    // Null terminator
-    buffer[length] = '\0';
-
-    return buffer;
-}
+char *readDataFromFile(char *filepath, size_t *size);
 
 #endif // TESTING_H
