d4107b12c768609ad6f8e08733770b6500772856
redstarbird <contact@redstarbird.com>
2026-07-19 21:55:54 +0100

    feat(generator): implement basic source map generation

--- src/Generator.c
+++ src/Generator.c
@@ -12,6 +12,9 @@ struct CSSGenerator
 
     // User provided configuration for the generator
     struct CSSGeneratorConfig *config;
+
+    // Tracks the current file position in the generated CSS file (for source map generation)
+    struct FilePosition genPosition;
 };
 
 // Append a string to the generator's buffer, resizing if necessary
@@ -38,6 +41,19 @@ static void GeneratorAppend(struct CSSGenerator *generator, const char *str, siz
 static void GeneratorAppendChar(struct CSSGenerator *generator, char c)
 {
     GeneratorAppend(generator, &c, 1);
+
+    if (generator->config != NULL && generator->config->generateSourceMap)
+    {
+        // Update the current file position for source map generation
+        if (c == '\n')
+        {
+            generator->genPosition.column = 0;
+        }
+        else
+        {
+            generator->genPosition.column++;
+        }
+    }
 }
 
 // Append a string view to the generator's buffer
@@ -75,8 +91,78 @@ static void GeneratorAppendIndentation(struct CSSGenerator *generator)
     }
 }
 
+// Base64 alphabet for source map generation
+static const char BASE64_ALPHABET[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
+
+static void GeneratorAppendVLQ(struct CSSGenerator *generator, int value)
+{
+    // If value is positive, shift left by 1
+    // If value is negative, shift left by 1 and set the least significant bit to 1
+    unsigned int vlq = (value < 0) ? ((-value) << 1) + 1 : (value << 1);
+
+    do
+    {
+        // Get the least significant 5 bits using a bitwise AND with 0x1F which is 00011111
+        unsigned int digit = vlq & 0x1F;
+
+        // Shift right by 5 bits for the next iteration
+        vlq >>= 5;
+
+        if (vlq > 0)
+        {
+            // If there are more digits, set the continuation bit (6th bit) using bitwise OR with 0x20 which is 00100000
+            digit |= 0x20;
+        }
+
+        // Append the corresponding base64 character to the source map buffer
+        GeneratorAppendChar(generator, BASE64_ALPHABET[digit]);
+
+    } while (vlq > 0);
+}
+
+// Struct to track the state of the source map generation
+struct SourceMapState
+{
+    int previousGeneratedColumn;
+    int previousSourceIndex;
+    int previousSourceLine;
+    int previousSourceColumn;
+};
+
+// Appends a mapping to the source map generator buffer
+void AppendMapping(struct CSSGenerator *generator, struct SourceMapState *state, int generatedColumn, int sourceIndex, int sourceLine, int sourceColumn)
+{
+    // If source maps are not being generated, generator will be NULL, so we can return immediately (also check state for safety)
+    if (generator == NULL || state == NULL)
+    {
+        return;
+    }
+
+    // Generated column (relative to previous generated column)
+    GeneratorAppendVLQ(generator, generatedColumn - state->previousGeneratedColumn);
+    state->previousGeneratedColumn = generatedColumn;
+
+    // Source index (relative to previous source index)
+    GeneratorAppendVLQ(generator, sourceIndex - state->previousSourceIndex);
+    state->previousSourceIndex = sourceIndex;
+
+    // Source line (relative to previous source line)
+    GeneratorAppendVLQ(generator, sourceLine - state->previousSourceLine);
+    state->previousSourceLine = sourceLine;
+
+    // Source column (relative to previous source column)
+    GeneratorAppendVLQ(generator, sourceColumn - state->previousSourceColumn);
+    state->previousSourceColumn = sourceColumn;
+
+    // Separate mappings with a comma
+    GeneratorAppendChar(generator, ',');
+}
+
 // Recursive function to generate CSS from the AST nodes
-void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapGenerator, struct ASTNode *node)
+void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapGenerator, struct ASTNode *node, struct SourceMapState *sourceMapState)
 {
     if (node == NULL || generator == NULL)
     {
@@ -86,13 +172,17 @@ void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapG
     struct ASTNode *current = node;
     while (current != NULL)
     {
+
         switch (current->type)
         {
         case CSS_NODE_STYLESHEET:
-            GenerateCSS(generator, sourceMapGenerator, current->data.stylesheet.children);
+            GenerateCSS(generator, sourceMapGenerator, current->data.stylesheet.children, sourceMapState);
             break;
-
         case CSS_NODE_RULESET:
+
+            // Append node's mapping to the source map
+            AppendMapping(sourceMapGenerator, sourceMapState, generator->genPosition.column, 0, current->position.column, current->position.line);
+
             // Append selector, opening brace, recursively append declarations, and closing brace
 
             // Append indentation for the ruleset, needed for nested rulesets
@@ -110,7 +200,7 @@ void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapG
             }
 
             generator->currentIndentDepth++;
-            GenerateCSS(generator, sourceMapGenerator, current->data.ruleset.declarations);
+            GenerateCSS(generator, sourceMapGenerator, current->data.ruleset.declarations, sourceMapState);
             generator->currentIndentDepth--;
 
             GeneratorAppendIndentation(generator);
@@ -132,6 +222,9 @@ void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapG
             break;
 
         case CSS_NODE_DECLARATION:
+
+            AppendMapping(sourceMapGenerator, sourceMapState, generator->genPosition.column, 0, current->position.column, current->position.line);
+
             // Add property value pair to the buffer
 
             GeneratorAppendIndentation(generator);
@@ -165,6 +258,8 @@ void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapG
         // Handle at-rule nodes
         case CSS_NODE_AT_RULE:
 
+            AppendMapping(sourceMapGenerator, sourceMapState, generator->genPosition.column, 0, current->position.column, current->position.line);
+
             // Apply indentation for the name and parameters of the at-rule
             GeneratorAppendIndentation(generator);
 
@@ -192,7 +287,7 @@ void GenerateCSS(struct CSSGenerator *generator, struct CSSGenerator *sourceMapG
 
                 // Apply indentation for block and recursively generate its contents
                 generator->currentIndentDepth++;
-                GenerateCSS(generator, sourceMapGenerator, current->data.at_rule.block);
+                GenerateCSS(generator, sourceMapGenerator, current->data.at_rule.block, sourceMapState);
                 generator->currentIndentDepth--;
 
                 // End brace for the at-rule block with proper indentation
@@ -242,12 +337,12 @@ struct ArrowCSSBuildResult *ArrowCSSBuildResultCreate(char *css, char *sourceMap
     return result;
 }
 
-struct ArrowCSSBuildResult *CSSGeneratorFinish(struct CSSGenerator *generator)
+char *CSSGeneratorFinish(struct CSSGenerator *generator)
 {
     // Add the final null terminator
     GeneratorAppendChar(generator, '\0');
 
-    return ArrowCSSBuildResultCreate(generator->buffer, NULL);
+    return generator->buffer;
 }
 
 void CSSGeneratorInit(struct CSSGenerator *generator, struct CSSGeneratorConfig *config)
@@ -274,13 +369,29 @@ struct ArrowCSSBuildResult *ArrowCSS_GenerateCSSFromAST(struct CSSAST *ast, stru
 
     struct CSSGenerator sourceMapGenerator;
 
+    struct SourceMapState sourceMapState = {0};
+
     // If source map generation is enabled, initialise the source map generator
     if (config->generateSourceMap)
     {
         CSSGeneratorInit(&sourceMapGenerator, NULL);
+        // Start the JSON
+        GeneratorAppend(&sourceMapGenerator, "{\"version\":3,\"mappings\":", 24);
     }
 
-    GenerateCSS(&generator, config->generateSourceMap ? &sourceMapGenerator : NULL, ast->root);
+    // Recursive CSS generate function
+    GenerateCSS(&generator, config->generateSourceMap ? &sourceMapGenerator : NULL, ast->root, &sourceMapState);
 
-    return CSSGeneratorFinish(&generator);
+    char *cssOutput = CSSGeneratorFinish(&generator);
+    char *sourceMapOutput = NULL;
+
+    // If source map generation enabled, finish the source map
+    if (config->generateSourceMap)
+    {
+        // Close the source map JSON object
+        GeneratorAppend(&sourceMapGenerator, "\"}", 2);
+        sourceMapOutput = CSSGeneratorFinish(&sourceMapGenerator);
+    }
+
+    return ArrowCSSBuildResultCreate(cssOutput, sourceMapOutput);
 }
